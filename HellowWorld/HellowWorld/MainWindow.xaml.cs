5547740083356824e51da4c08476772fbfbc3e98
Edujesseyang <edujesseyang@gmail.com>
2025-04-13 21:48:31 -0700

    add

---
+++ HellowWorld/HellowWorld/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace HellowWorld
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            if (btn == bt1)
+            {
+                MessageBox.Show("You clicked   wrong botton!", "Oh, NO", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else if (btn == bt2)
+            {
+                MessageBox.Show("Yoho! You clicked the right button, Good job.", "Good job", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            else if (btn == bt3)
+            {
+                MessageBox.Show("Hello, World", "Great job", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+
+        }
+    }
+}
